ef000677c556c20d0468039e3e521d458ce6a90c
Parth Kesar <parthkesar@gmail.com>
2024-01-23 02:22:36 -0800

    Contact Me backend post handling done.

--- app.js
+++ app.js
@@ -9,10 +9,28 @@ const port = 3000;
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.set('trust proxy', true);
+
 
 // Set up a route for the home page
 app.get('/', (req, res) => {
+
+    const clientIP = req.ip || req.connection.remoteAddress;
+    console.log(clientIP);
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+
+app.post("/contact", (req, res)=>{
+    let name = req.body.companyName;
+    let mail = req.body.email;
+    let message = req.body.Message;
+
+    console.log(name);
+    console.log(mail);
+    console.log(message);
+
+    res.send("Submitted");
 });
 
 
