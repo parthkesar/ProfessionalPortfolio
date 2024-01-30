fc83aa28e8b474b1ff6b6886a635706b985933cc
Parth Kesar <parthkesar@gmail.com>
2024-01-29 21:24:14 -0800

    Hyperlinks to Social Media Created.

--- app.js
+++ app.js
@@ -16,45 +16,43 @@ app.set('trust proxy', true);
 
 //DATABASE
 const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    password: 'P@rth123',
-    database: 'myPersonalPortfolio'
-  });
+  host: 'localhost',
+  user: 'root',
+  password: 'P@rth123',
+  database: 'myPersonalPortfolio'
+});
 
 
 // Set up a route for the home page
 app.get('/', (req, res) => {
 
-    const clientIP = req.ip || req.connection.remoteAddress;
-    console.log(clientIP);
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 
-app.post("/contact", (req, res)=>{
-    const name = req.body.companyName;
-    const mail = req.body.email;
-    const message = req.body.Message;
+app.post("/contact", (req, res) => {
+  const name = req.body.companyName;
+  const mail = req.body.email;
+  const message = req.body.Message;
 
 
-    const sql = `insert into contact (Organisation_Name, Email, Message, Send_Date) value ("${name}", "${mail}", "${message}", current_date());`;
+  const sql = `insert into contact (Organisation_Name, Email, Message, Send_Date) value ("${name}", "${mail}", "${message}", current_date());`;
 
-    pool.query(sql, (error, results, fields) => {
-        if (error) {
-          console.error(error);
-          res.send(error);
-        } else {
-            res.send("Submitted");
-        }
-        pool.end();
-      });
+  pool.query(sql, (error, results, fields) => {
+    if (error) {
+      console.error(error);
+      res.send(error);
+    } else {
+      res.sendFile(path.join(__dirname, 'public', 'index.html'));
+    }
+    pool.end();
+  });
 
-    
+
 });
 
 
 // Start the server
 app.listen(port, () => {
-    console.log(`Server is running at http://localhost:${port}`);
+  console.log(`Server is running at http://localhost:${port}`);
 });
